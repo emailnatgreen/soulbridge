53e75fb2e656f36c997c618c13b56d49831950c1
Nathan Green <emailnatgreen@gmail.com>
2026-03-01 23:51:16 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -183,7 +183,7 @@ export default function Home() {
               {/* Skills & Training */}
               <DropdownMenu>
                 <DropdownMenuTrigger asChild>
-                  <Button variant="ghost" className="text-indigo-300/80 hover:bg-indigo-500/10 hover:text-indigo-300">
+                  <Button variant="ghost" className="text-indigo-600 hover:bg-indigo-100 hover:text-indigo-700">
                     <GraduationCap className="w-4 h-4 mr-2" />
                     Skills <ChevronDown className="w-3 h-3 ml-1" />
                   </Button>
