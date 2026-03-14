664ddb58eee2200b524cd9df17470c1ef0880670
Nathan Spencer Green <emailnatgreen@gmail.com>
2026-03-14 22:59:54 +0000

    File changes

--- src/pages/Axi.jsx
+++ src/pages/Axi.jsx
@@ -5,7 +5,6 @@ import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Send, Sparkles, Loader2, ArrowLeft, UserPlus } from 'lucide-react';
 import { Link } from 'react-router-dom';
-import { createPageUrl } from '../utils';
 import MessageBubble from '../components/MessageBubble';
 import AddAgentModal from '../components/AddAgentModal';
 
