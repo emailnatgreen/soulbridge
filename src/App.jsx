220109de5d95b75d6ebfdd144346a843fab0be89
Nathan Spencer Green <emailnatgreen@gmail.com>
2026-03-14 22:57:28 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -61,6 +61,8 @@ const AuthenticatedApp = () => {
         />
       ))}
       <Route path="/AxiCommandDashboard" element={<LayoutWrapper currentPageName="AxiCommandDashboard"><AxiCommandDashboard /></LayoutWrapper>} />
+      <Route path="/AgentLeaderboard" element={<LayoutWrapper currentPageName="AgentLeaderboard"><AgentLeaderboard /></LayoutWrapper>} />
+      <Route path="/ReputationHistoryLog" element={<LayoutWrapper currentPageName="ReputationHistoryLog"><ReputationHistoryLog /></LayoutWrapper>} />
       <Route path="*" element={<PageNotFound />} />
     </Routes>
   );
