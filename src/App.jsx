34e2d08c8d6fcb5f4199bd4f9857faf45ed3e7a0
Nathan Spencer Green <emailnatgreen@gmail.com>
2026-03-15 06:59:37 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-d
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
-import Layout from './layout';
+import Layout from './Layout';
 
 // Page imports
 import Home from './pages/Home';
