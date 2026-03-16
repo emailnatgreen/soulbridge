151b5924ab7450df8031133510bdadff2c2022e8
Nathan Spencer Green <emailnatgreen@gmail.com>
2026-03-16 14:31:52 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 import VillageCalendar from './pages/VillageCalendar';
+import AxiCommandDashboard from './pages/AxiCommandDashboard';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
