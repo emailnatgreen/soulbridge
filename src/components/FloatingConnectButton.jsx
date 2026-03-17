b232625b66a6a702e9d6ebe726dad5ed32068df4
Nathan Spencer Green <emailnatgreen@gmail.com>
2026-03-17 18:52:06 +0000

    File changes

--- src/components/FloatingConnectButton.jsx
+++ src/components/FloatingConnectButton.jsx
@@ -105,7 +105,7 @@ export default function FloatingConnectButton() {
 
   if (!isOpen) {
     return (
-      <div className="fixed bottom-6 right-6 z-40 flex gap-2">
+      <div className="fixed bottom-6 right-6 md:bottom-6 sm:bottom-20 z-40 flex gap-2">
         <Button
           onClick={() => {
             setIsOpen(true);
