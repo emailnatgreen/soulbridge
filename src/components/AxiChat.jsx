50e84b1917aa7fc6eacf61b3ec2b9cb6b1fe587b
Nathan Spencer Green <emailnatgreen@gmail.com>
2026-03-17 16:59:15 +0000

    File changes

--- src/components/AxiChat.jsx
+++ src/components/AxiChat.jsx
@@ -100,15 +100,7 @@ const AxiChat = memo(function AxiChat({ isOpen, setIsOpen }) {
         }
         setConversation(convo);
         setMessages(convo.messages || []);
-        // Populate activeAgents from persisted participant_agent_ids
-        if (convo.participant_agent_ids && convo.participant_agent_ids.length > 0) {
-          const agents = await Promise.all(
-            convo.participant_agent_ids.map(id => base44.entities.Agent.get(id))
-          );
-          setActiveAgents(agents.filter(Boolean));
-        } else {
-          setActiveAgents([]);
-        }
+        setActiveAgents([]);
         if (unsubscribeRef.current) unsubscribeRef.current();
         unsubscribeRef.current = base44.agents.subscribeToConversation(convo.id, (data) => {
           setMessages([...data.messages]);
