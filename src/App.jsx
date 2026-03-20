8ca41c14703d7cc5242e5fbcca18de253a7143bb
Nathan Spencer Green <emailnatgreen@gmail.com>
2026-03-20 09:01:45 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const LayoutWrapper = ({ children, currentPageName }) => Layout ?
   : <>{children}</>;
 
 const AuthenticatedApp = () => {
-  const { isLoadingAuth, isLoadingPublicSettings, authError, navigateToLogin } = useAuth();
+  const { isLoadingAuth, isLoadingPublicSettings, authError, navigateToLogin, isAuthenticated } = useAuth();
 
   // Show loading spinner while checking app public settings or auth
   if (isLoadingPublicSettings || isLoadingAuth) {
