f4d5b67949f05121accb942f049d0337fece167c
Nathan Spencer Green <emailnatgreen@gmail.com>
2026-03-23 16:47:03 +0000

    File changes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -78,6 +78,7 @@ export default function Landing() {
     delete window.__soulbridge.identity;
     setDidConnected(null);
     setDid('');
+    navigate('/');
   };
 
   const resetInactivityTimer = () => {
