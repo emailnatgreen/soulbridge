2bf2e6eadacb7593c905cd4887f033c928562b8a
Nathan Spencer Green <emailnatgreen@gmail.com>
2026-04-05 10:22:06 +0000

    File changes

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -136,7 +136,7 @@ export default function Landing() {
     if (inactivityRef.current) clearTimeout(inactivityRef.current);
     inactivityRef.current = setTimeout(() => {
       handleDisconnectDID();
-    }, 5 * 60 * 1000);
+    }, 2 * 60 * 1000);
   };
 
   useEffect(() => {
