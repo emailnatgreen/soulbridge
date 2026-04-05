398e503bb2be1609f780bd2947951ac48438cf79
Nathan Spencer Green <emailnatgreen@gmail.com>
2026-04-05 18:24:59 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { queryClientInstance } from '@/lib/query-client'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import PageNotFound from './lib/PageNotFound';
 import { AuthProvider, useAuth } from '@/lib/AuthContext';
+import { OwnerGovernorProvider } from '@/lib/OwnerGovernorContext';
 import UserNotRegisteredError from '@/components/UserNotRegisteredError';
 
 // Layout
@@ -376,10 +377,12 @@ function App() {
   return (
     <AuthProvider>
       <QueryClientProvider client={queryClientInstance}>
-        <Router>
-          <AuthenticatedApp />
-        </Router>
-        <Toaster />
+        <OwnerGovernorProvider>
+          <Router>
+            <AuthenticatedApp />
+          </Router>
+          <Toaster />
+        </OwnerGovernorProvider>
       </QueryClientProvider>
     </AuthProvider>
   );
